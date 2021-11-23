42909a2273a9300531d6c4317680aac0899c6e92
Reckerth Daniel <reckerthdaniel@gmail.com>
2021-11-23 17:26:37 +0200

    changed url

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "https://ds2021-energy-daniel-backend.herokuapp.com/api/auth/";
 
 // possible need to add the other fields
 const register = (fullName, username, password, birthday, address) => {
